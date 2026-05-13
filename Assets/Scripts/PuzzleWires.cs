9ccaa4918b1159ab5a3daaede271f3ffa0389057
DESKTOP-6HR0FGG\davew <davewilson260@gmail.com>
2026-05-13 15:34:19 -0700

    made the colors change on wires

--- Assets/Scripts/PuzzleWires.cs
+++ Assets/Scripts/PuzzleWires.cs
@@ -8,21 +8,23 @@ using UnityEngine.UI;
 public class PuzzleWires : MonoBehaviour
 {
     public GameObject Wire1, Wire2, Wire3, Wire4, Wire5, Wire6;
-    private List<GameObject> wires = new List<GameObject>();
-    private List<GameObject> reds = new List<GameObject>();
-    private List<GameObject> blues = new List<GameObject>();
-    private List<GameObject> greens = new List<GameObject>();
+    private List<GameObject> leftWires = new List<GameObject>();
+    private List<GameObject> rightWires = new List<GameObject>();
+    private int redCount = 0;
+    private int blueCount = 0;
+    private int greenCount = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        wires.Add(Wire1);
-        wires.Add(Wire2);
-        wires.Add(Wire3);
-        wires.Add(Wire4);
-        wires.Add(Wire5);
-        wires.Add(Wire6);
-        ColorCalc();
+        leftWires.Add(Wire1);
+        leftWires.Add(Wire2);
+        leftWires.Add(Wire3);
+        rightWires.Add(Wire4);
+        rightWires.Add(Wire5);
+        rightWires.Add(Wire6);
+        ColorCalcLeft();
+        ColorCalcRight();
     }
 
     // Update is called once per frame
@@ -31,32 +33,120 @@ public class PuzzleWires : MonoBehaviour
         
     }
 
-    private void ColorCalc()
-    { 
-        for (int i = 0; i < wires.Count; i++)
+    private void ColorCalcLeft()
+    {
+        bool running = true;
+        while (running)
         {
+            for (int i = 0; i < leftWires.Count; i++)
+            {
+
                 int color = Random.Range(0, 3);
-                if (color == 0 && reds.Count < 2)
+                if (color == 0 && redCount < 1 && leftWires[i].GetComponent<Image>().color == Color.white)
                 {
-                    wires[i].GetComponent<Image>().color = Color.red;
-                    reds.Add(wires[i]);
+                    leftWires[i].GetComponent<Image>().color = Color.red;
+                    redCount += 1;
+                }
+
+                else if (color == 0 && redCount >= 1)
+                {
+                    color = Random.Range(1, 3);
                 }
 
 
-                if (color == 1 && blues.Count < 2)
+                if (color == 1 && blueCount < 1 && leftWires[i].GetComponent<Image>().color == Color.white)
                 {
-                    wires[i].GetComponent<Image>().color = Color.blue;
-                    blues.Add(wires[i]);
+                    leftWires[i].GetComponent<Image>().color = Color.blue;
+                    blueCount += 1;
                 }
-
-                if (color == 2 && greens.Count < 2)
+                else if (color == 1 && blueCount >= 1)
                 {
-                    wires[i].GetComponent<Image>().color = Color.green;
-                    greens.Add(wires[i]);
-                }
+                    int coin = Random.Range(0, 2);
+                    if (coin == 0)
+                    {
+                        color = 0;
+                    }
+                    else
+                    {
+                        color = 2;
 
+                    }
+
+                    if (color == 2 && greenCount < 1 && leftWires[i].GetComponent<Image>().color == Color.white)
+                    {
+                        leftWires[i].GetComponent<Image>().color = Color.green;
+                        greenCount += 1;
+                    }
+                    else if (color == 2 && greenCount >= 1)
+                    {
+                        color = Random.Range(0, 2);
+                    }
+
+                }
+            }
+            if (redCount == 1 && blueCount == 1 && greenCount == 1)
+            {
+                running = false;
+            }
         }
     }
 
+    private void ColorCalcRight()
+    {
+        bool running = true;
+        while (running)
+        {
+            for (int i = 0; i < rightWires.Count; i++)
+            {
+
+                int color = Random.Range(0, 3);
+                if (color == 0 && redCount < 2 && rightWires[i].GetComponent<Image>().color == Color.white)
+                {
+                    rightWires[i].GetComponent<Image>().color = Color.red;
+                    redCount += 1;
+                }
+
+                else if (color == 0 && redCount >= 2)
+                {
+                    color = Random.Range(1, 3);
+                }
+
+
+                if (color == 1 && blueCount < 2 && rightWires[i].GetComponent<Image>().color == Color.white)
+                {
+                    rightWires[i].GetComponent<Image>().color = Color.blue;
+                    blueCount += 1;
+                }
+                else if (color == 1 && blueCount >= 2)
+                {
+                    int coin = Random.Range(0, 2);
+                    if (coin == 0)
+                    {
+                        color = 0;
+                    }
+                    else
+                    {
+                        color = 2;
+
+                    }
+
+                    if (color == 2 && greenCount < 2 && rightWires[i].GetComponent<Image>().color == Color.white)
+                    {
+                        rightWires[i].GetComponent<Image>().color = Color.green;
+                        greenCount += 1;
+                    }
+                    else if (color == 2 && greenCount >= 2)
+                    {
+                        color = Random.Range(0, 2);
+                    }
+
+                }
+            }
+            if (redCount == 2 && blueCount == 2 && greenCount == 2)
+            {
+                running = false;
+            }
+        }
+    }
 
 }
