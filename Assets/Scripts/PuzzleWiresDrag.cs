c3b2acd7e8d17cacfe0f123c9cf5968e843cabe0
MUKILTEO\1722453 <davewilson260@gmail.com>
2026-05-19 12:43:35 -0700

    working on line renderer

--- Assets/Scripts/PuzzleWiresDrag.cs
+++ Assets/Scripts/PuzzleWiresDrag.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -6,34 +9,39 @@ using UnityEngine.WSA;
 
 public class PuzzleWiresDrag : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
-    private GameObject self;
+    private GameObject clonee;
     private bool hovered = false;
     private RectTransform canvas;
 
-
+    //
+    // private UILineRenderer lR;
 
     //public static 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-        self = gameObject;
-        //canvas = GameObject.Find("Puzzle-Wires").transform;
-    }
+    //void Start()
+    //{
+    //    lR = GetComponent<UILineRenderer>();
+    //    //canvas = GameObject.Find("Puzzle-Wires").transform;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //self.transform.position = Mouse.current.position.ReadValue();
-    }
+    //}
 
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //        lR.points[1] = Input.mousePosition;
+
+    //}
     public void WireGrab()
     {
         if (hovered)
         {
             RectTransform rt = transform as RectTransform;
-            var clone = Instantiate(self, rt.position, transform.rotation, rt.parent.parent as RectTransform);
-            //clone.GetComponent<RectTransform>().
+            var clone = Instantiate(gameObject, rt.position, transform.rotation, rt.parent.parent as RectTransform);
+            clone.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 50);
+            clone.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 50);
+            clonee = clone;
+
             Debug.Log("Wire Grabbed");
         }
 
